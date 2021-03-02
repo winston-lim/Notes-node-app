be4f6bed3a92922a004e5b67b480d2ef4e18620f
winston-lim <winston_lim1@hotmail.com>
2021-03-02 18:59:52 +0800

    Build loadNotes utility function

--- notes.js
+++ notes.js
@@ -1,4 +1,15 @@
-function getNotes() {
-  return 'Notes...';
+const fs = require('fs');
+const getNotes = function() {
+  console.log('Notes...')
 }
-module.exports = getNotes;
+const loadNotes = function() {
+  //data is a buffer as the return value is in binary, not a string
+  const dataBuffer = fs.readFileSync('notes.json');
+  //dataBuffer.toString() converts it from binary representation to a string
+  const dataJSON = JSON.parse(dataBuffer.toString());
+}
+
+
+module.exports = {
+  getNotes: getNotes,
+}
