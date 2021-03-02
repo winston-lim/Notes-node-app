b417cdda865188eebfbb577ff5ae96fbeaee794a
winston-lim <winston_lim1@hotmail.com>
2021-03-02 19:24:57 +0800

    Update addNote utility function

--- notes.js
+++ notes.js
@@ -6,11 +6,19 @@ const addNote = function (title, body) {
   const data = loadNotes();
   //recall if no data exists yet, data =[]
   //if data exists, it will be Array<Object>
+
+  //to prevent duplicate titles, if title already exists, add it to a duplicateNotes array
+  const duplicateNotes = data.filter((note)=>note.title === title);
+  if (duplicateNotes.length >0) {
+    console.log('title taken')
+    return;
+  }
   data.push({
     title: title,
     body: body,
   });
   updateNotes(data);
+  console.log('new note added')
 };
 const loadNotes = function () {
   try {
@@ -18,6 +26,7 @@ const loadNotes = function () {
     const dataBuffer = fs.readFileSync("notes.json");
     //dataBuffer.toString() converts it from binary representation to a string
     const dataJSON = JSON.parse(dataBuffer.toString());
+    return dataJSON;
   } catch (err) {
     //if notes.json does not exist, create an array to store in our file
     return [];
