c5c88bbf2942c164ed4abebed4aba5dcde40fc8f
winston-lim <winston_lim1@hotmail.com>
2021-03-02 19:11:23 +0800

    Update loadNotes utility function

--- notes.js
+++ notes.js
@@ -1,15 +1,18 @@
-const fs = require('fs');
-const getNotes = function() {
-  console.log('Notes...')
-}
-const loadNotes = function() {
-  //data is a buffer as the return value is in binary, not a string
-  const dataBuffer = fs.readFileSync('notes.json');
-  //dataBuffer.toString() converts it from binary representation to a string
-  const dataJSON = JSON.parse(dataBuffer.toString());
-}
-
-
+const fs = require("fs");
+const getNotes = function () {
+  console.log("Notes...");
+};
+const loadNotes = function () {
+  try {
+    //data is a buffer as the return value is in binary, not a string
+    const dataBuffer = fs.readFileSync("notes.json");
+    //dataBuffer.toString() converts it from binary representation to a string
+    const dataJSON = JSON.parse(dataBuffer.toString());
+  } catch (err) {
+    //if notes.json does not exist, create an array to store in our file
+    return [];
+  }
+};
 module.exports = {
   getNotes: getNotes,
-}
+};
